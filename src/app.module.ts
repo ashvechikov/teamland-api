06c86c9a1f8db99e98cd6ca13e5b557d30fe4a61
shvechikov <alex@shvechikov.ru>
2024-02-11 23:17:33 +0300

    initial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BlogModule } from './modules/blog/blog.module';
 import { BlogController } from './modules/blog/blog.controller';
-import { BlogService } from './modules/blog/service/blog.service';
 
 @Module({
   imports: [BlogModule],
   controllers: [BlogController],
-  providers: [BlogService],
 })
 export class AppModule {}
