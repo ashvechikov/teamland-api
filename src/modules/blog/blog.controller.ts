da90d9f164f4e37a36fe3a1d8da0d6e56222f4d2
Alex Shvechikov <shvechikov.a.a@gmail.com>
2024-02-12 18:37:36 +0300

    initial

--- src/modules/blog/blog.controller.ts
+++ src/modules/blog/blog.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { IBlogService } from './blog.interface';
 
 @Controller()
 export class BlogController {
   constructor(private readonly service: IBlogService) {}
+
+  @Get()
+  get(id: number) {
+    this.service.getBlog(id);
+  }
 }
